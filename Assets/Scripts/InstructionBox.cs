39685820212fb0ecffc0e5c13d09666cbabd1a4b
Amani <88464204+slurpinpuffs@users.noreply.github.com>
2023-03-11 14:21:40 -0700

    Added instructional box
    Added instructional box for dialogue controls

---
+++ Assets/Scripts/InstructionBox.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionBox : InteractableObject
+{
+    GameObject self;
+
+    protected override void Start(){
+        z_Collider = GetComponent<Collider2D>();
+        self = this.gameObject;
+    }
+
+    protected override void OnInteract(){
+        if(!z_Interacted){
+            z_Interacted = true;
+            self.SetActive(false);
+        }
+    }
+}
